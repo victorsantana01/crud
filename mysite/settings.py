5f3886f7cc36a9708f8345998e846bba2e7a5abb
victorsantana <victorsantana.it@gmail.com>
2021-07-28 00:48:21 -0300

    add.

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud_django',
+        'USER': 'root',
+        'PASSWORD': 'auto.sup',
     }
 }
 
